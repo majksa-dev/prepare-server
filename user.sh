90b4b09fc964c86e6c360c79ff939410144b5662
Ondřej Maxa <47477160+maxa-ondrej@users.noreply.github.com>
2024-03-06 00:55:44 +0100

    feat: add user setup

---
+++ user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install Oh My ZSH
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install PowerLevel10k
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+# Setup .dotfiles
+git clone https://github.com/majksa-dev/dotfiles.git .dotfiles
+cd .dotfiles
+stow .
+cd -
