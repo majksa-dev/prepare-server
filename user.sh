68245582533803af0694686e298b98ba5c875292
Ondřej Maxa <47477160+maxa-ondrej@users.noreply.github.com>
2024-03-06 01:06:46 +0100

    fix: ZSH_CUSTOM

--- user.sh
+++ user.sh
@@ -4,6 +4,7 @@
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install PowerLevel10k
+ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
